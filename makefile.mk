5327581cb403acddedde81631ce755fd0f49c0cb
J. Jonathan (J.J.) Jones <jj.jones@live.com>
2019-08-07 23:01:09 -0400

    Update makefile.mk

--- makefile.mk
+++ makefile.mk
@@ -7,6 +7,15 @@ install:
 test:
 	python -m pytest -vv --cov=myrepolib tests/*.py
 	python -m pytest --nbval notebook.ipynb
+	
+validate-circleci:
+        circleci config process .circleci/config.yml
+
+run-circleci-local:
+         circleci local execute
+
+lint: # This line should already be there with regular pylint
+        hadolint path/to/Dockerfile
 
 
 lint:
